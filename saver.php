0b1666ecc9bf806bcd04dd8ad44c6e31070e2164
Sveder <ms@svedersoft.com>
2012-05-11 03:59:17 +0300

    1. Autostart music. 2. Replay now works. 3. Added fivecon

--- saver.php
+++ saver.php
@@ -3,19 +3,16 @@
     mysql_selectdb("freshpaint");
     
     //Get the last id:
-    $last_id_query = mysql_query("select max(id) from sessions;", $db);
+    $last_id_query = mysql_query("select max(id) as last from sessions;", $db);
 
     $last_id_row = mysql_fetch_assoc($last_id_query);
-    $last_id = $last_id_row[0];
-    echo $last_id;
-    
-    
+    $last_id = $last_id_row["last"];
     $new_id = intval($last_id) + 1;
     
     //Convert it to a number in base 36 which basically adds the letters to get
     //a shorther alphanumeric representation:
     $link_text = base_convert($new_id, 10, 36);
-    $recreate = $_POST["data"];
+    $recreate = $_REQUEST["data"];
     
     $recreate = base64_encode(mysql_real_escape_string($recreate));
     
